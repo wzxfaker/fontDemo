b4ed70c93fa5a0f1f33fa4956776c36edd7f511c
wangzexin <wangzexin@inke.cn>
2024-07-10 18:45:26 +0800

    删除杂乱代码

--- FontDemo/ViewController.m
+++ FontDemo/ViewController.m
@@ -11,6 +11,22 @@
 #import "UIView+Image.h"
 #import "ColorSliderView.h"
 
+#ifndef weakify
+    #if __has_feature(objc_arc)
+    #define weakify(object) __weak __typeof__(object) weak##_##object = object;
+    #else
+    #define weakify(object) __block __typeof__(object) block##_##object = object;
+    #endif
+#endif
+
+#ifndef strongify
+    #if __has_feature(objc_arc)
+    #define strongify(object) __typeof__(object) object = weak##_##object;
+    #else
+    #define strongify(object) __typeof__(object) object = block##_##object;
+    #endif
+#endif
+
 @interface ViewController ()
 
 ///<##>
@@ -33,13 +49,13 @@
     particleTextView.text = @"你好";
     particleTextView.font = [UIFont systemFontOfSize:150 weight:UIFontWeightBold];
     particleTextView.particleColor = [UIColor redColor];
-//    particleTextView.backgroundColor = [UIColor yellowColor];
+    particleTextView.backgroundColor = [UIColor yellowColor];
     particleTextView.backgroundColor = [UIColor clearColor];
     self.particleTextView = particleTextView;
     [self.view addSubview:particleTextView];
 
     
-    
+    //字体添加背景图片
     UILabel *label = [[UILabel alloc] initWithFrame:CGRectMake((self.view.frame.size.width-300)*0.5, 430, 300, 200)];
     NSMutableAttributedString *attrStr = [[NSMutableAttributedString alloc] initWithString:@"啦啦啦"];
     [attrStr addAttribute:NSFontAttributeName value:[UIFont systemFontOfSize:120 weight:UIFontWeightBold] range:NSMakeRange(0, attrStr.length)];
@@ -49,42 +65,21 @@
     label.hidden = YES;
     [self.view addSubview:label];
     
-    
+    //获取截图展示的imageView
     self.imageView = [[UIImageView alloc] init];
     self.imageView.frame = label.frame;
     [self.view addSubview:self.imageView];
     
     self.colorSliderView = [[ColorSliderView alloc] initWithFrame:CGRectMake(0, CGRectGetMaxY(self.imageView.frame), self.view.frame.size.width, 30)];
+    weakify(self);
     self.colorSliderView.touchedColor = ^(UIColor * _Nonnull color) {
+        strongify(self);
         self.particleTextView.particleColor = color;
         UIImage *image = [self.particleTextView convertToImage];
         self.imageView.image = image;
     };
     [self.view addSubview:self.colorSliderView];
-    
-    
-    UILongPressGestureRecognizer *longG = [[UILongPressGestureRecognizer alloc] initWithTarget:self action:@selector(handleLongPress)];
-//    [self.view addGestureRecognizer:longG];
-    
-}
 
-- (void)handleLongPress {
-    UIColor *color = [UIColor colorWithRed:(arc4random() % 256)*1.0/256 green:(arc4random() % 256)*1.0/256 blue:(arc4random() % 256)*1.0/256 alpha:1.0];
-    self.particleTextView.particleColor = color;
-    self.particleTextView.font = [UIFont systemFontOfSize:arc4random() % 50+100 weight:UIFontWeightBold];
-    self.particleTextView.text = @"我很好";
-    UIImage *image = [self.particleTextView convertToImage];
-    self.imageView.image = image;
-}
-
-- (void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event {
-    [super touchesBegan:touches withEvent:event];
-    UIColor *color = [UIColor colorWithRed:(arc4random() % 256)*1.0/256 green:(arc4random() % 256)*1.0/256 blue:(arc4random() % 256)*1.0/256 alpha:1.0];
-    self.particleTextView.particleColor = color;
-    self.particleTextView.font = [UIFont systemFontOfSize:120 weight:UIFontWeightBold];
-    self.particleTextView.text = @"我很好";
-    UIImage *image = [self.particleTextView convertToImage];
-    self.imageView.image = image;
 }
 
 
